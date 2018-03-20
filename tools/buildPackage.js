99072da18827072d1b815c6d31bc6040b0bde09e
Pranjal <pranjal@brave.com>
2018-03-20 12:05:41 -0700

    Bundling tor with Brave

--- tools/buildPackage.js
+++ tools/buildPackage.js
@@ -72,6 +72,21 @@ if (channel !== 'dev') {
 }
 
 const buildDir = appName + '-' + process.platform + '-' + arch
+const torS3Prefix = 'https://s3.us-east-2.amazonaws.com/demo-tor-binaries/'
+
+var sha512Tor
+if (isDarwin) {
+  sha512Tor = 'af09b7f31fcb3996db3883dff0c20187fdb9653fb5d207dc7e8d3d8cd9de90f2908e47f4414c03b5633a94183425e0c9804316ece353f8174ec4897559932a4e  '
+} else if (isLinux) {
+  sha512Tor = 'cdcac1dbd9a1d2865cb61cb3c0d6540196d4dd576f86fcbcbd70713f75d56bf8407e931056bcba5395506cf80b4f51ed620cc9bb0c43fda6c6730d87152b51b3  '
+} else {
+  sha512Tor = '0fbd88d590069fdc243fcdcc84b8aa10caa921fc11d7e776334f0cbd5b0095b21046adfd291be61dc414c232bc1ff22e7e8142b0af1d20e71154f8de66be83ab  '
+}
+
+var torURL = torS3Prefix + 'tor-' + process.platform
+if (isWindows) {
+  torURL += '.zip'
+}
 
 console.log('Writing buildConfig.js...')
 config.writeBuildConfig(
@@ -154,8 +169,25 @@ if (isLinux) {
   cmds.push('copy .\\res\\start-tile-150.png "' + path.join(buildDir, 'resources', 'start-tile-150.png') + '"')
   cmds.push('makensis.exe -DARCH=' + arch + ` res/${channel}/braveDefaults.nsi`)
   cmds.push('ncp ./app/extensions ' + path.join(buildDir, 'resources', 'extensions'))
+
   // Make sure the Brave.exe binary is squirrel aware so we get squirrel events and so that Squirrel doesn't auto create shortcuts.
   cmds.push(`"node_modules/rcedit/bin/rcedit.exe" ./${appName}-win32-` + arch + `/${appName}.exe --set-version-string "SquirrelAwareVersion" "1"`)
+}
+
+// Verify tor binaries and bundle with Brave
+var torPath
+if (isDarwin) {
+  torPath = path.join(buildDir, `${appName}.app`, 'Contents', 'Resources', 'extensions', 'bin')
+} else {
+  torPath = path.join(buildDir, 'resources', 'extensions', 'bin')
+}
+
+cmds.push('mkdirp ' + torPath)
+cmds.push('curl -o ' + path.join(torPath, 'tor') + ' ' + torURL)
+cmds.push('shasum -a 512 -c <<< "' + sha512Tor + path.join(torPath, 'tor') + '"')
+
+if (isWindows) {
+  cmds.push('unzip ' + path.join(torPath, 'tor') + ' -d ' + path.join(buildDir, 'resources', 'extensions', 'bin'))
 }
 
 if (isDarwin) {
